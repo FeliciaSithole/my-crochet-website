e3dbfafce07e9a46b8d9f74505ae8ad7e9a6736c
FeliciaSithole <feliciasithole.fs@gmail.com>
2025-09-14 15:09:02 +0200

    added contact me information

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ const successMessage = document.getElementById('success-message');
 if(contactForm) {
     contactForm.addEventListener('submit', (e) => {
         e.preventDefault();
+
         fetch(contactForm.action, {
             method: 'POST',
             body: new FormData(contactForm),
@@ -29,7 +30,16 @@ if(contactForm) {
         }).then(response => {
             if(response.ok){
                 successMessage.style.display = 'block';
+                successMessage.style.opacity = '1'; 
                 contactForm.reset();
+
+                setTimeout(() => {
+                    successMessage.style.opacity = '0';
+                    setTimeout(() => {
+                        successMessage.style.display = 'none';
+                    }, 500); 
+                }, 5000);
+
             } else {
                 alert('Oops! There was a problem submitting your form.');
             }
